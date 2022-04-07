21e14d2024402b47bec428ec2ab63e38eea07f8c
Sander Buruma <sanderburuma@fastrmail.nl>
2022-04-07 16:41:20 +0200

    favicon added plus working static folder

--- __init__.py
+++ __init__.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template 
-app = Flask(__name__, template_folder='templates') 
+from flask import Flask, render_template
 
-@app.route("/") 
-def hello(): 
+app = Flask(
+    __name__,
+    template_folder='templates',
+    static_url_path='',
+    static_folder='static'
+)
+
+
+@app.route("/")
+def hello():
     return render_template("index.html")
 
-if __name__ == "__main__": 
-    app.run()
 
+if __name__ == "__main__":
+    app.run()
